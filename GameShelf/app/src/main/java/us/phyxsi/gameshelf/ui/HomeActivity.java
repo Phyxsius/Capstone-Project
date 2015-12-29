142109f957e7e9c239912e425752b30685020dc5
Andy <thingswithhandles@gmail.com>
2015-12-29 15:25:57 -0600

    fix: home screen padding

--- GameShelf/app/src/main/java/us/phyxsi/gameshelf/ui/HomeActivity.java
+++ GameShelf/app/src/main/java/us/phyxsi/gameshelf/ui/HomeActivity.java
@@ -1,14 +1,29 @@
 package us.phyxsi.gameshelf.ui;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewStub;
+import android.view.WindowInsets;
+import android.view.animation.AnimationUtils;
+import android.widget.ActionMenuView;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toolbar;
@@ -29,6 +44,9 @@ public class HomeActivity extends Activity {
     private GridLayoutManager layoutManager;
     @BindInt(R.integer.num_columns) int columns;
 
+    // data
+//    private FeedAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,8 +59,52 @@ public class HomeActivity extends Activity {
 
         setActionBar(toolbar);
         if (savedInstanceState == null) {
-//            animateToolbar();
+            animateToolbar();
         }
+
+        // drawer layout treats fitsSystemWindows specially so we have to handle insets ourselves
+        drawer.setOnApplyWindowInsetsListener(new View.OnApplyWindowInsetsListener() {
+            @Override
+            public WindowInsets onApplyWindowInsets(View v, WindowInsets insets) {
+                // inset the toolbar down by the status bar height
+                ViewGroup.MarginLayoutParams lpToolbar = (ViewGroup.MarginLayoutParams) toolbar
+                        .getLayoutParams();
+                lpToolbar.topMargin += insets.getSystemWindowInsetTop();
+                lpToolbar.rightMargin += insets.getSystemWindowInsetRight();
+                toolbar.setLayoutParams(lpToolbar);
+
+                // inset the grid top by statusbar+toolbar & the bottom by the navbar (don't clip)
+//                grid.setPadding(grid.getPaddingLeft(),
+//                        insets.getSystemWindowInsetTop() + ViewUtils.getActionBarSize
+//                                (HomeActivity.this),
+//                        grid.getPaddingRight() + insets.getSystemWindowInsetRight(), // landscape
+//                        grid.getPaddingBottom());
+
+                // inset the fab for the navbar
+                ViewGroup.MarginLayoutParams lpFab = (ViewGroup.MarginLayoutParams) fab
+                        .getLayoutParams();
+                lpFab.bottomMargin += insets.getSystemWindowInsetBottom(); // portrait
+                lpFab.rightMargin += insets.getSystemWindowInsetRight(); // landscape
+                fab.setLayoutParams(lpFab);
+
+                // we place a background behind the status bar to combine with it's semi-transparent
+                // color to get the desired appearance.  Set it's height to the status bar height
+                View statusBarBackground = findViewById(R.id.status_bar_background);
+                FrameLayout.LayoutParams lpStatus = (FrameLayout.LayoutParams)
+                        statusBarBackground.getLayoutParams();
+                lpStatus.height = insets.getSystemWindowInsetTop();
+                statusBarBackground.setLayoutParams(lpStatus);
+
+                // clear this listener so insets aren't re-applied
+                drawer.setOnApplyWindowInsetsListener(null);
+
+                return insets.consumeSystemWindowInsets();
+            }
+        });
+        setupTaskDescription();
+
+        checkEmptyState();
+        checkConnectivity();
     }
 
     private int gridScrollY = 0;
@@ -78,5 +140,89 @@ public class HomeActivity extends Activity {
 //        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void checkEmptyState() {
+        if (0 == 0) {
+            loading.setVisibility(View.VISIBLE);
+
+            // ensure grid scroll tracking/toolbar z-order is reset
+            gridScrollY = 0;
+            toolbar.setTranslationZ(0f);
+        } else {
+            loading.setVisibility(View.GONE);
+        }
+    }
+
+    private void checkConnectivity() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        boolean connected = activeNetworkInfo != null && activeNetworkInfo.isConnected();
+        if (!connected) {
+            loading.setVisibility(View.GONE);
+            ViewStub stub = (ViewStub) findViewById(R.id.stub_no_connection);
+            ImageView iv = (ImageView) stub.inflate();
+            final AnimatedVectorDrawable avd =
+                    (AnimatedVectorDrawable) getDrawable(R.drawable.avd_no_connection);
+            iv.setImageDrawable(avd);
+            avd.start();
+        }
+    }
+
+    private void setupTaskDescription() {
+        // set a silhouette icon in overview as the launcher icon is a bit busy
+        // and looks bad on top of colorPrimary
+        //Bitmap overviewIcon = ImageUtils.vectorToBitmap(this, R.drawable.ic_launcher_silhouette);
+        // TODO replace launcher icon with a monochrome version from RN.
+        Bitmap overviewIcon = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+        setTaskDescription(new ActivityManager.TaskDescription(getString(R.string.app_name),
+                overviewIcon,
+                ContextCompat.getColor(this, R.color.primary)));
+        overviewIcon.recycle();
+    }
+
+    private void animateToolbar() {
+        // this is gross but toolbar doesn't expose it's children to animate them :(
+        View t = toolbar.getChildAt(0);
+        if (t != null && t instanceof TextView) {
+            TextView title = (TextView) t;
+
+            // fade in and space out the title.  Animating the letterSpacing performs horribly so
+            // fake it by setting the desired letterSpacing then animating the scaleX ¯\_(ツ)_/¯
+            title.setAlpha(0f);
+            title.setScaleX(0.8f);
+
+            title.animate()
+                    .alpha(1f)
+                    .scaleX(1f)
+                    .setStartDelay(300)
+                    .setDuration(900)
+                    .setInterpolator(AnimationUtils.loadInterpolator(this,
+                            android.R.interpolator.fast_out_slow_in));
+        }
+        View amv = toolbar.getChildAt(1);
+        if (amv != null & amv instanceof ActionMenuView) {
+            ActionMenuView actions = (ActionMenuView) amv;
+            popAnim(actions.getChildAt(0), 500, 200); // filter
+            popAnim(actions.getChildAt(1), 700, 200); // overflow
+        }
+    }
+
+    private void popAnim(View v, int startDelay, int duration) {
+        if (v != null) {
+            v.setAlpha(0f);
+            v.setScaleX(0f);
+            v.setScaleY(0f);
+
+            v.animate()
+                    .alpha(1f)
+                    .scaleX(1f)
+                    .scaleY(1f)
+                    .setStartDelay(startDelay)
+                    .setDuration(duration)
+                    .setInterpolator(AnimationUtils.loadInterpolator(this, android.R.interpolator
+                            .overshoot));
+        }
     }
 }
